b660223e3276893a3e84c1fec90395c7e0ce72a0
MPiotrowska <monikapiotrowska27@gmail.com>
2020-10-23 17:53:14 +0200

    social links

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -4,13 +4,13 @@ import instagram from '../img/social/instagram.svg'
 export const SocialLink = (props) => {
   return (
     <a
-      className={`navbar-item ${props.className}`}
+      className={`navbar-item`}
       href={props.href}
       target="_blank"
       rel="noopener noreferrer"
     >
       <span className="icon">
-        <img src={props.src} alt={props.alt} {...props} />
+        <img src={props.src} alt={props.alt} {...props} className={`${props.className}`} />
       </span>
     </a>
   );
